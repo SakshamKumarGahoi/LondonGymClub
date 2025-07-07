0be29335d96bbdb172e7fad8fb4b24866bc9978e
Saksham Kumar Gahoi <sakshamgahoi123@gmail.com>
2025-07-08 00:31:25 +0530

    building main structure

---
+++ londongymclub/tailwind.config.js
@@ -0,0 +1,11 @@
+content: ["./index.html", "./src/**/*.{js,jsx}"],
+theme: {
+  extend: {
+    colors: {
+      primary: "#E50914",
+      darkGrey: "#2E2E2E",
+      jetBlack: "#0D0D0D",
+    },
+  },
+},
+plugins: [],
